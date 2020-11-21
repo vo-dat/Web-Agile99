a621efe8e6a3ecceaaa87ef47ce424cdbaf8a964
thuyhuong210 <63534790+thuyhuong210@users.noreply.github.com>
2020-11-21 23:16:56 +0700

    Update Edit.php
    Đã chỉnh sửa file chỉnh sửa nhóm thành viên

--- Web_5AE/View/Group/Edit.php
+++ Web_5AE/View/Group/Edit.php
@@ -1,9 +1,9 @@
-<div>
-<h2><span><a href="adminManager.php?mod=group&act=edit&id=<?php echo $id; ?>">Chỉnh sửa nhóm thành viên</a></span></h2>
+<div class="container my-5">
+<h2><span><a style="text-decoration: none" href="adminManager.php?mod=group&act=edit&id=<?php echo $id; ?>">Chỉnh sửa nhóm thành viên</a></span></h2>
 
 <form action="adminManager.php?mod=group&act=edit&id=<?php echo $id; ?>" method="post" class="form">
-	<p><label>Tên nhóm</label><input type="text" name="txtGroupName" id="txtGroupName" value="<?php echo $row['GroupName']; ?>"/></p>
-    <p><label>&nbsp;</label><input type="submit" name="btnChange" id="btnChange" value="Đổi thông tin" /></p>
+	<p><label class="mr-3" style="font-weight: bold">Tên nhóm:</label><input type="text" name="txtGroupName" id="txtGroupName" value="<?php echo $row['GroupName']; ?>"/></p>
+    <p><label>&nbsp;</label><input type="submit" name="btnChange" id="btnChange" value="Đổi thông tin" class="btn btn-primary px-3" /></p>
 </form>
 
 </div>
